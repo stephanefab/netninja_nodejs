956a88100203c0d5b2d69edd9af2d623f58da736
stephanefab <kouadiostephanefabienbrou@gmail.com>
2025-05-06 16:31:18 +0000

    `Added routing and navigation to server and views`

--- server.js
+++ server.js
@@ -3,15 +3,48 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    console.log("url: ", req.url, " method: ", req.method, " status: ", res.statusCode);
     res.setHeader('Content-Type', 'text/html');
+    uri = req.url;
+    method = req.method;
 
-    fs.readFile('./views/index.html', (err, data) => {
-        if(err){
-            res.end('<p>Error</p>');
+    if (method === "GET") {
+        // index: /
+        if (uri === '/') {
+            fs.readFile('./views/index.html', (err, data) => {
+                res.end(data);
+            });
         }
-        res.end(data);
-    })
+        // about: /about
+        else if (uri === '/about') {
+            fs.readFile('./views/about.html', (err, data) => {
+                res.end(data);
+            });
+        }
+        // contact: /contact
+        else if (uri === '/contact') {
+            fs.readFile('./views/contact.html', (err, data) => {
+                res.end(data);
+            });
+        }
+        // profil : /profil
+        else if (uri === '/profil') {
+            fs.readFile('./views/profil.html', (err, data) => {
+                res.end(data);
+            });
+        }
+        else {
+            res.statusCode = 404;
+            fs.readFile('./views/404.html', (err, data) => {
+                res.end(data);
+            });
+        }
+    } else {
+        res.statusCode = 405;
+        fs.readFile('./views/405.html', (err, data) => {
+            res.end(data);
+        });
+    }
+
 });
 
 host = '127.0.0.1';
