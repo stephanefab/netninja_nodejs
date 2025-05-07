6d2d4596fd4d9eb8d87e0f00842159dba335cd3f
bfabien99 <fabienbrou99@gmail.com>
2025-05-07 10:45:29 +0000

    `Added routing for multiple pages and 404 error handling using express`

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ const app = express(); // Create an instance of the express app
 
 // routing
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    //res.send("Hello World");
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname});
+});
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+app.get('/contact', (req, res) => {
+    res.sendFile('./views/contact.html', {root: __dirname});
+});
+app.get('/profil', (req, res) => {
+    res.sendFile('./views/profil.html', {root: __dirname});
 });
 
+// 404 page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname});
+});
 // start server
 app.listen(5500);
